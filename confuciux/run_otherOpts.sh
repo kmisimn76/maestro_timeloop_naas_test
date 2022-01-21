5dc9f9fb63d61c545d0e3e4743be8345070685a5
kmisimn76 <gmlakd4u@uos.ac.kr>
2022-01-21 15:20:08 +0900

    NAAS with timeloop

--- confuciux/run_otherOpts.sh
+++ confuciux/run_otherOpts.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 cd src/Other_Opts
 #python main.py --outdir outdir --model vgg16 --fitness latency --cstr area --mul 0.5 --epochs 100000 --df shi --alg genetic #VGG genetic example
 python main.py --outdir outdir --model vgg16 --fitness latency --cstr area --mul 0.5 --epochs 100000 --df shi --alg genetic
